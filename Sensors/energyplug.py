06de1b4bc2fd39f508fca4d7c9ef34dd94c802bc
vagus010 <70719414+vagus010@users.noreply.github.com>
2021-04-29 22:09:37 -0400

    Update energyplug.py

--- Sensors/energyplug.py
+++ Sensors/energyplug.py
@@ -2,9 +2,6 @@
 # Credit to Blake Hartshorn and his article here (https://www.blakehartshorn.com/monitoring-and-graphing-with-etekcity-vesync/) for the base code to use
 
 
-# To enable the program on startup I used sudo nano /etc/rc.local to open rc.local and modified it by adding a line python /home/pi/energy.py &
-# Modifying rc.local will ensure that the program runs on start up 
-
 from pyvesync_v2.vesync import VeSync
 import time
 import sys
@@ -12,13 +9,14 @@ import datetime
 import Adafruit_DHT
 from influxdb import InfluxDBClient
 
+time.sleep(35)
 # Configure InfluxDB connection variables
-host = "169.254.196.152" # My Ubuntu NUC
+host = "10.0.0.61" # My Ubuntu NUC
 port = 8086 # default port
 user = "rpi-4" # the user/password created for the pi, with write access
 password = "rpi-4" 
 dbname = "monitor" # the database we created earlier
-interval = 3 # Sample period in seconds
+interval = 15 # Sample period in seconds
 
 # Create the InfluxDB client object
 client = InfluxDBClient(host, port, user, password, dbname)
@@ -31,7 +29,10 @@ manager.login()
 manager.update()
 
 
-switch = manager.devices[0]
+switch0 = manager.devices[0]
+switch1 = manager.devices[1]
+switch2 = manager.devices[2]
+switch3 = manager.devices[3]
 
 # think of measurement as a SQL table, it's not...but...
 measurement = "rpi-moni"
@@ -43,7 +44,10 @@ try:
     while True:
         # Read the sensor using the configured driver and gpio
         iso = time.ctime()
-        ener = switch.get_power() 
+        ener0 = switch0.get_power()
+	ener1 = switch1.get_power() 
+	ener2 = switch2.get_power() 
+	ener3 = switch3.get_power() 
         # Print for debugging, uncomment the below line
         # Create the JSON data structure
         data = [
@@ -54,14 +58,16 @@ try:
               },
               "time": iso,
               "fields": {
-                  "energy" : ener,             
+                  "energy0" : ener0,
+		  "energy1" : ener1,
+		  "energy2" : ener2,
+		  "energy3" : ener3,
  }
           }
         ]
         # Send the JSON data to InfluxDB
 	client.write_points(data)
         # Wait until it's time to query again...
-	print(ener)
         time.sleep(interval)
 
 except KeyboardInterrupt:
