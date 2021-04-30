b644643c823a8d604fb4fe75090eda09e0fc6adc
vagus010 <70719414+vagus010@users.noreply.github.com>
2021-04-29 22:15:43 -0400

    Create launchtemp.sh

---
+++ Sensors/launchtemp.sh
@@ -0,0 +1,10 @@
+# This is a file that runs for startup by a custom service that needs to be coded and implemented
+# Credit to this github thread for the help: https://raspberrypi.stackexchange.com/questions/78991/running-a-script-after-an-internet-connection-is-established/79033
+
+#!/bin/sh
+# launchtemp.sh
+
+cd /
+cd home/pi
+sudo python tempsensor.py
+cd /
